cbabbb932439939c6c3f6c183c22d4cd87858851
Sorpirit <danvuk11@gmail.com>
2019-11-16 15:47:53 +0200

    Init

---
+++ Z_TDS_V1/Assets/Script/PlayerMovment.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovment : MonoBehaviour
+{
+
+    public float speed;
+    public float maxSpeed;
+
+    private float horAxes;
+    private float verAxes;
+
+    private Rigidbody2D myRb;
+
+    private void Start()
+    {
+        myRb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horAxes = Input.GetAxis("Horizontal");
+        verAxes = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector2 direction = new Vector2(horAxes, verAxes).normalized;
+        Debug.Log(direction);
+        myRb.velocity = direction * speed;
+    }
+}
