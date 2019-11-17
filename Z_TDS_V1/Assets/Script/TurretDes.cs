b1f344a93884d24fe5a2370f919814e478d55db8
nikinov <novellenicholas@gmail.com>
2019-11-17 21:55:54 +0100

    ...

---
+++ Z_TDS_V1/Assets/Script/TurretDes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretDes : MonoBehaviour
+{
+    Collision2D col;
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        col = coll;
+        StartCoroutine(Wait());
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10);
+        if (col.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
